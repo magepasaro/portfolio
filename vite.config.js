10e54f3239845a24015ccf5b56d81fb940939077
Maria Rodrigues <magepasaro@gmail.com>
2025-09-02 02:12:37 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: "public",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: "../dist",
   },
 });
